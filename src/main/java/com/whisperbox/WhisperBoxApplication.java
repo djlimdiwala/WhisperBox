fc1b6170f1a8c9aadde0826b6fe0af247d9004bc
DHAVAL LIMDIWALA <dhaval.limdiwala@gmail.com>
2026-07-24 09:31:04 +0000

    Initial Spring Boot setup

---
+++ src/main/java/com/whisperbox/WhisperBoxApplication.java
@@ -0,0 +1,19 @@
+package com.whisperbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                FlywayAutoConfiguration.class
+        }
+)
+public class WhisperBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhisperBoxApplication.class, args);
+    }
+}
